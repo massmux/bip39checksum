b05108cf2e0af1c5db75ba3b3e1eeb376fa8d5fb
massmux <go@massmux.com>
2021-06-03 11:56:32 +0200

    sequence file as par

--- bip39checksum.py
+++ bip39checksum.py
@@ -23,31 +23,46 @@
 
 
 import mnemonic
-import sys
+import sys,argparse
 
-(wordsfile, sequence)=("english.txt","23words.txt")
+wordsfile="english.txt"
 
-try:
-    print("Reading BIP39 words list file..")
+""" parsing arguments """
+def parseArguments():
+    global args
+    parser = argparse.ArgumentParser("bip39checksum.py")
+    parser.add_argument("-s","--sequence", help="Specify sequence words file \
+                        to check. Single line, words separed by a space", type=str, required=True)
+    args = parser.parse_args()
+
+
+def main():
+  try:
     f=open(wordsfile)
     english = f.read().strip().split('\n')
     f.close()
-except:
+  except:
     print("Error while reading BIP39 words list file")
     sys.exit()
     
-try:
-    print("Reading your 23 words BIP39 sequence to check..")
+  try:
     f=open(sequence)
     words = f.read().strip()
     f.close()
-except:
+  except:
     print("Error reading your 23 words BIP39 sequence to check")
     sys.exit()
 
-m = mnemonic.Mnemonic('english')
-print("::Tested correct checksum sequences::")
-for word in english:
+  m = mnemonic.Mnemonic('english')
+  print("::Tested correct checksum sequences::")
+  for word in english:
     seq = "%s %s" % (words,word)
     if m.check(seq):
         print (seq)
+
+
+if __name__ == "__main__":
+    parseArguments()
+    (sequence)=(args.sequence)
+    main()
+
